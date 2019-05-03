24b9dcf2962cf677c2681f3e48950565b7feb7cb
Bram Mittendorff <botw44@gmail.com>
2019-05-03 16:32:30 +0200

    Added ingress support (#7)
    * Added ingress support
    
    * Removed entry because it is not used and Added manifest.json
    
    * Added default start screen

--- mopidy/rootfs/etc/cont-init.d/11-nginx.sh
+++ mopidy/rootfs/etc/cont-init.d/11-nginx.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Community Hass.io Add-ons: Mopidy
-# Configures NGINX for use with code-server
+# Community Hass.io Add-ons: InfluxDB
+# Configures NGINX for use with the Chronograf
 # ==============================================================================
+declare port
 declare certfile
+declare ingress_interface
+declare ingress_port
+declare ingress_entry
 declare keyfile
 
-mkdir -p /var/log/nginx
+port=$(bashio::addon.port 80)
+ingress_entry=$(bashio::addon.ingress_entry)
+if bashio::var.has_value "${port}"; then
+    bashio::config.require.ssl
 
-if bashio::config.true 'ssl'; then
-    certfile=$(bashio::config 'certfile')
-    keyfile=$(bashio::config 'keyfile')
+    if bashio::config.true 'ssl'; then
+        certfile=$(bashio::config 'certfile')
+        keyfile=$(bashio::config 'keyfile')
 
-    sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx-ssl.conf
-    sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx-ssl.conf
+        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
+        sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/servers/direct.conf
+        sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/servers/direct.conf
+
+    else
+        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
+    fi
+
+    sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/direct.conf
 fi
 
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
