4ada519e53b60ed5a1906efb989e7ceb397e1ed6
Bram <botw44@gmail.com>
2019-05-03 13:48:36 +0200

    Added ingress support

--- mopidy/rootfs/etc/cont-init.d/10-requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Community Hass.io Add-ons: Mopidy
-# This files check if all user configuration requirements are met
-# ==============================================================================
-bashio::config.require.ssl
-
