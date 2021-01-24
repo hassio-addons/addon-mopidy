d2d86a39af513c2c19d65f0c9b086976c47db86d
Franck Nijhof <git@frenck.dev>
2021-01-25 00:23:39 +0100

    Restructure init scripts

--- mopidy/rootfs/etc/cont-init.d/packages.sh
+++ mopidy/rootfs/etc/cont-init.d/packages.sh
@@ -3,12 +3,20 @@
 # Home Assistant Community Add-on: Mopidy
 # Install user configured/requested packages
 # ==============================================================================
+
 if bashio::config.has_value 'system_packages'; then
     apt update \
         || bashio::exit.nok 'Failed updating Ubuntu packages repository indexes'
 
     for package in $(bashio::config 'system_packages'); do
         apt install -y "$package" \
+            || bashio::exit.nok "Failed installing package ${package}"
+    done
+fi
+
+if bashio::config.has_value 'python_packages'; then
+    for package in $(bashio::config 'python_packages'); do
+        pip install "$package" \
             || bashio::exit.nok "Failed installing package ${package}"
     done
 fi
