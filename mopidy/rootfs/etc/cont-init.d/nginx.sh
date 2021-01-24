6ab35a719f2014858bdb588ce67012dfbdb6bb6d
Franck Nijhof <git@frenck.dev>
2021-01-25 00:15:08 +0100

    Refactor NGINX configuration and HA authentication

---
+++ mopidy/rootfs/etc/cont-init.d/nginx.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Mopidy
+# Configures NGINX
+# ==============================================================================
+
+# Generate Ingress configuration
+bashio::var.json \
+    interface "$(bashio::addon.ip_address)" \
+    entry "^$(bashio::addon.ingress_entry)" \
+    | tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
+
+# Generate direct access configuration, if enabled.
+if bashio::var.has_value "$(bashio::addon.port 80)"; then
+    bashio::config.require.ssl
+    bashio::var.json \
+        certfile "$(bashio::config 'certfile')" \
+        keyfile "$(bashio::config 'keyfile')" \
+        leave_front_door_open "^$(bashio::config 'leave_front_door_open')" \
+        port "^$(bashio::addon.port 80)" \
+        ssl "^$(bashio::config 'ssl')" \
+        | tempio \
+            -template /etc/nginx/templates/direct.gtpl \
+            -out /etc/nginx/servers/direct.conf
+fi
--- mopidy/rootfs/etc/nginx/templates/ingress.gtpl
+++ mopidy/rootfs/etc/nginx/templates/ingress.gtpl
@@ -1,5 +1,5 @@
 server {
-    listen %%interface%%:%%port%% default_server;
+    listen {{ .interface }}:8099 default_server;
 
     include /etc/nginx/includes/server_params.conf;
     include /etc/nginx/includes/proxy_params.conf;
@@ -11,8 +11,8 @@ server {
         proxy_pass http://backend/;
         sub_filter_once off;
         sub_filter_types *;
-        sub_filter '/iris/'  '%%ingress_entry%%/iris/';
-        sub_filter '/mopidy/ws' '%%ingress_entry%%/mopidy/ws';
+        sub_filter '/iris/'  '{{ .entry }}/iris/';
+        sub_filter '/mopidy/ws' '{{ .entry }}/mopidy/ws';
         sub_filter 'href="manifest.json">' 'href="manifest.json" crossorigin="use-credentials">';
         sub_filter 'document.body.appendChild(js);' 'document.body.appendChild(js); (function() { var wait=function(){let t=document.querySelector(".sidebar__menu__item");t?t.click():setTimeout(function(){wait()},100)};wait(); })();';
     }
