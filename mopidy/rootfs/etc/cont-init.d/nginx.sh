26b78d8e7ff44797e120e83e88e74d182c9d50df
Bram Mittendorff <botw44@gmail.com>
2021-01-26 13:25:43 +0100

    Removed ^ as requested by Frenck
    This will fix issue: https://github.com/hassio-addons/addon-mopidy/issues/90

--- mopidy/rootfs/etc/cont-init.d/nginx.sh
+++ mopidy/rootfs/etc/cont-init.d/nginx.sh
@@ -7,7 +7,7 @@
 # Generate Ingress configuration
 bashio::var.json \
     interface "$(bashio::addon.ip_address)" \
-    entry "^$(bashio::addon.ingress_entry)" \
+    entry "$(bashio::addon.ingress_entry)" \
     | tempio \
         -template /etc/nginx/templates/ingress.gtpl \
         -out /etc/nginx/servers/ingress.conf
